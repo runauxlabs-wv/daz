3254a36b2835623f85006c05a46578ad075e556f
design-daz <90227415+design-daz@users.noreply.github.com>
2021-09-15 13:29:44 +0900

    portfolio
    210915

---
+++ js/210915_index_0.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+    //------------sec1로 스크롤 이동 버튼 ----//
+
+$(".gnb > li:first-child").each(function() {
+    var sec1 = $("." + $(this).data('id')).offset().top;
+    
+
+    $(this).click(function() {
+        $("html, body").animate({
+            scrollTop: sec1
+        }, 1000);
+        $(this).addClass('on');
+    });
+});
+
+
+//------------sec2로 스크롤 이동 버튼 ----//
+
+$(".gnb > li:nth-child(2)").each(function() {
+    var sec2 = $("." + $(this).data('id')).offset().top;
+    
+
+    $(this).click(function() {
+        $("html, body").animate({
+            scrollTop: sec2
+        }, 1000);
+        $(this).addClass('on');
+    });
+});
+
+
+//------------sec3로 스크롤 이동 버튼 ----//
+
+$(".gnb > li:nth-child(3)").each(function() {
+    var sec3 = $("." + $(this).data('id')).offset().top;
+    
+
+    $(this).click(function() {
+        $("html, body").animate({
+            scrollTop: sec3
+        }, 1000);
+        $(this).addClass('on');
+    });
+});
+
+//------------sec4로 스크롤 이동 버튼 ----//
+
+
+$(".gnb > li:last-child").each(function() {
+    var sec4 = $("." + $(this).data('id')).offset().top;
+    
+
+    $(this).click(function() {
+        $("html, body").animate({
+            scrollTop: sec4
+        }, 1000);
+        $(this).addClass('on');
+    });
+});
+
+//------------heart ----//
+
+// $(".sns a:nth-child(1)").mouseover(function() {
+//     $(".sns a:nth-child(1) span").addClass('.sns_none').siblings().removeClass('.sns_none');
+    
+
+// });
+
+
+});
