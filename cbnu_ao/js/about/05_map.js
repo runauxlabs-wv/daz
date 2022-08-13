a21d76a54f4c674715891fa51319ad696238bde6
design-daz <90227415+design-daz@users.noreply.github.com>
2022-08-13 14:46:37 +0900

    map.js++
    +++

--- cbnu_ao/js/about/05_map.js
+++ cbnu_ao/js/about/05_map.js
@@ -20,22 +20,37 @@ $(document).ready(function () {
     // =========================== //
 
 
-    var map;
+    // var map;
 
-    function initMap() {
-      var seoul = { lat: 37.5642135 ,lng: 127.0016985 };
-      map = new google.maps.Map( document.getElementById('#g_map'), {
-          zoom: 12,
-          center: seoul
-        });
+    // function myMap() {
+    //   var seoul = { lat: 37.5642135 ,lng: 127.0016985 };
+    //   map = new google.maps.Map( document.getElementById('#g_map'), {
+    //       zoom: 12,
+    //       center: seoul
+    //     });
     
-      new google.maps.Marker({
-        position: seoul,
-        map: map,
-        label: "서울 중심 좌표"
-      });
-    };
+    //   new google.maps.Marker({
+    //     position: seoul,
+    //     map: map,
+    //     label: "서울 중심 좌표"
+    //   });
+    // };
 
+    function myMap(){
+        // 2-2. 구글 맵의 옵션 
+        var mapOptions = { 
+              // 지도의 중앙 위치 : 영국 런던
+              center:new google.maps.LatLng(51.508742, -0.120850)
+              // 줌 레벨 : 5
+                 , zoom:5
+        };
+        
+        // 2. 구글 지도 생성(만들기)
+        // var map = new google.maps.Map(맵캔버스, 맵옵션들 );
+        var map = new google.maps.Map( 
+               document.getElementById("g_map") 
+              , mapOptions );
+     }
 
 
 
