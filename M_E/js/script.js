ac0baa9eb471debc6790f27e2bcc4bb8a8c6e5f5
Daz <90227415+design-daz@users.noreply.github.com>
2022-08-22 21:54:02 +0900

    220822 js update
    resize

--- M_E/js/script.js
+++ M_E/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    
+    //리사이징 할때마다 새로고침
+    var lastWidth = $(window).width();
+    $(window).resize(function () {
+        if ($(window).width() != lastWidth) {
+            location.reload();
+            lastWidth = $(window).width();
+            return false;
+        }
+    });
+
 
     // 모달 메뉴 열기 //
     $(".menu").click(function () {
