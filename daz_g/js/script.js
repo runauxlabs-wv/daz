bed08325c2675aefa5a609edf9adace5fabdfa7b
Daz <90227415+design-daz@users.noreply.github.com>
2022-09-01 20:25:13 +0900

    daz_g js update
    resize

--- daz_g/js/script.js
+++ daz_g/js/script.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
 
 
     //리사이징 할때마다 새로고침
-    // var lastWidth = $(window).width();
-    // $(window).resize(function () {
-    //     if ($(window).width() != lastWidth) {
-    //         location.reload();
-    //         lastWidth = $(window).width();
-    //         return false;
-    //     }
-    // });
+    var lastWidth = $(window).width();
+    $(window).resize(function () {
+        if ($(window).width() != lastWidth) {
+            location.reload();
+            lastWidth = $(window).width();
+            return false;
+        }
+    });
 
 
     //------------sec1로 스크롤 이동 버튼 ----//
