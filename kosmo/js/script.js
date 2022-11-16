e0df359f9d5abf32a9003ae4cadd716561c90a59
Daz <90227415+design-daz@users.noreply.github.com>
2022-11-16 15:53:33 +0900

    221116 tablet
    re

--- kosmo/js/script.js
+++ kosmo/js/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function () {
+
+
+  //리사이징 할때마다 새로고침
+      // var lastWidth = $(window).width();
+      // $(window).resize(function () {
+      //     if ($(window).width() != lastWidth) {
+      //         location.reload();
+      //         lastWidth = $(window).width();
+      //         return false;
+      //     }
+      // });
+
+
   // ====== Nav ===== //
 
   $("nav ul.gnb >li").hover(
@@ -72,6 +85,72 @@ $(document).ready(function () {
 
     
   });
+
+
+
+  // Tablet
+  // =========================== //
+
+    if ($(window).width() < 1441) {
+
+      var swiper_02 = new Swiper(".mySwiper.swp_02", {
+        slidesPerView: 4,
+        spaceBetween: 0,
+    
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+      });
+
+      
+
+
+      // $("ul.gnb>li").click(function(){
+      //     $(this).find("ul.sub").stop().slideToggle();
+      
+      // });
+
+
+      // // 모바일메뉴 open/close
+      // $(".menu_m").click(function () {
+      //     $(".gnb_s, .sub_col, .menu_m_x").stop().addClass("on");
+      //     $(".sub_bg_bg").show();
+      //     $(".menu_m").hide();
+      //     // .gnb에 on클래스 추가/삭제    
+      // });
+      // $(".menu_m_x").click(function () {
+      //     $(".gnb_s, .sub_col, .menu_m_x").stop().removeClass("on");
+      //     $(".menu_m").show();
+      //     $(".sub_bg_bg").hide();
+
+
+      //     // .gnb에 on클래스 추가/삭제
+      // });
+
+
+
+      // 모바일 메뉴 화살표 방향변경 
+
+      // $("a .m_arr").click(function(){
+      //     $("a .m_arr_u").addClass("on");
+
+      //     $("a .m_arr").removeClass("on");
+
+      // });
+      
+      // $("a .m_arr_u").click(function(){
+      //     $("a .m_arr_u").removeClass("on");
+      //     $("a .m_arr").addClass("on");
+
+      // });
+
+  };
+
+
+
+
 
 
   // scroll top 버튼
