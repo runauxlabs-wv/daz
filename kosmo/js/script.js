1dd5a8528995901a833b3dd96b361fd3ff21fc9d
Daz <90227415+design-daz@users.noreply.github.com>
2022-11-16 18:04:56 +0900

    221116_m update
    hahaha

--- kosmo/js/script.js
+++ kosmo/js/script.js
@@ -103,53 +103,42 @@ $(document).ready(function () {
         },
 
       });
-
-      
-
-
-      // $("ul.gnb>li").click(function(){
-      //     $(this).find("ul.sub").stop().slideToggle();
-      
-      // });
-
-
-      // // 모바일메뉴 open/close
-      // $(".menu_m").click(function () {
-      //     $(".gnb_s, .sub_col, .menu_m_x").stop().addClass("on");
-      //     $(".sub_bg_bg").show();
-      //     $(".menu_m").hide();
-      //     // .gnb에 on클래스 추가/삭제    
-      // });
-      // $(".menu_m_x").click(function () {
-      //     $(".gnb_s, .sub_col, .menu_m_x").stop().removeClass("on");
-      //     $(".menu_m").show();
-      //     $(".sub_bg_bg").hide();
-
-
-      //     // .gnb에 on클래스 추가/삭제
-      // });
-
-
-
-      // 모바일 메뉴 화살표 방향변경 
-
-      // $("a .m_arr").click(function(){
-      //     $("a .m_arr_u").addClass("on");
-
-      //     $("a .m_arr").removeClass("on");
-
-      // });
-      
-      // $("a .m_arr_u").click(function(){
-      //     $("a .m_arr_u").removeClass("on");
-      //     $("a .m_arr").addClass("on");
-
-      // });
+   
 
   };
 
+  // Mobile
+  // =========================== //
 
 
+  if ($(window).width() < 361) {
+
+    var swiper_02 = new Swiper(".mySwiper.swp_02", {
+      slidesPerView: 3,
+      spaceBetween: 20,
+  
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+    });
+
+    var swiper_04 = new Swiper(".mySwiper.swp_04", {
+      slidesPerView: 3,
+      spaceBetween: 40,
+      // slidesPerGroup: 3,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction : false,
+        pauseOnMouseEnter : true,
+      },
+      
+    });
+
+  };
 
 
 
