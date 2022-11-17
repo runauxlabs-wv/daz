76c5fe589902fe1644e11afeb8ecf0e9d2f4a848
Daz <90227415+design-daz@users.noreply.github.com>
2022-11-17 13:22:00 +0900

    re
    re

--- kosmo/js/script.js
+++ kosmo/js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-  //리사이징 할때마다 새로고침
-  // var lastWidth = $(window).width();
-  // $(window).resize(function () {
-  //     if ($(window).width() != lastWidth) {
-  //         location.reload();
-  //         lastWidth = $(window).width();
-  //         return false;
-  //     }
-  // });
+  // 리사이징 할때마다 새로고침
+  var lastWidth = $(window).width();
+  $(window).resize(function () {
+      if ($(window).width() != lastWidth) {
+          location.reload();
+          lastWidth = $(window).width();
+          return false;
+      }
+  });
 
   // ====== Nav ===== //
 
