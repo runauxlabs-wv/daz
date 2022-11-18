ebc82b4acab46eca065bd00b81f1d3ee5d6a2478
Daz <90227415+design-daz@users.noreply.github.com>
2022-11-18 16:33:29 +0900

    221118_ euclid
    .

---
+++ euclid/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+
+
+
+  // scroll top 버튼
+  // =========================== //
+  $("#top").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+
+  
+});
