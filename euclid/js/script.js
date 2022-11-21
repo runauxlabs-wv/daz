25e3ff4c2cde2812c88ea9ad3f77ad6433579e9c
Daz <90227415+design-daz@users.noreply.github.com>
2022-11-21 18:03:29 +0900

    221121
    z

--- euclid/js/script.js
+++ euclid/js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
-
-
-
-
-  // scroll top 버튼
+  // scroll top button
   // =========================== //
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#top").show();
+    } else {
+      $("#top").hide();
+    }
+  });
+
   $("#top").click(function () {
     $("html, body").animate(
       {
@@ -14,5 +19,22 @@ $(document).ready(function () {
     );
   });
 
-  
+  // Swiper menu
+  // =========================== //
+
+  var swiper = new Swiper(".mySwiper.swp_01", {
+    slidesPerView: 5,
+    slidesPerGroup: 1,
+    spaceBetween: 0,
+    loop: true,
+
+  });
+
+  $(".swp_01_w > div.swp_01_s").click(function () {
+    $(this).addClass('on').siblings().removeClass('on');
+    $("#" + $(this).data('id')).addClass('on').siblings().removeClass('on');
+  });
+
+
+
 });
